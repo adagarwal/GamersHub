6446efb73abcd944f4285176b74b66aed1c34f8f
adagarwal <agarwal3@uci.edu>
2020-11-25 16:25:56 -0800

    Logos and button functionality

--- src/client/src/components/dashboard/Dashboard.js
+++ src/client/src/components/dashboard/Dashboard.js
@@ -9,10 +9,15 @@ import StreamersCarousel from "./StreamersCarousel.js";
 import AccountLogo from './AccountLogo.png';
 import HomeLogo from './HomeLogo.png';
 import HelpLogo from './HelpLogo.png';
+import SteamLogo from './SteamLogo.png';
+import EpicLogo from './EpicLogo.png';
+import TwitchLogo from './TwitchLogo.png';
 import GameLogo from './GameLogo.png';
 import StreamLogo from './StreamLogo.png';
 
 //import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+
+const sampleData = new Array(2).fill("Test");
 
 class Dashboard extends Component {
   onLogoutClick = (e) => {
@@ -21,6 +26,22 @@ class Dashboard extends Component {
   };
   render() {
     const { user } = this.props.auth;
+    const gotoHome = () => {
+        window.alert("HOME!");
+    }
+    const gotoSteam = () => {
+        window.alert("STEAM!");
+    }
+    const gotoEpic = () => {
+        window.alert("EPIC!");
+    }
+    const gotoTwitch = () => {
+        window.alert("TWITCH!");
+    }
+    const gotoHelp = () => {
+        window.alert("HELP!");
+    }
+
     return (
       <div >
         <div className="row" style={{backgroundColor: "#99aab5"}}>
@@ -48,7 +69,7 @@ class Dashboard extends Component {
                 </h5>
               </div>
               <div className="col s1 right-align">
-                <img src={AccountLogo} style={{ height: "8vh", marginLeft: "2vh", marginTop: "2vh"}}></img>
+                  <img src={AccountLogo} style={{ height: "8vh", marginLeft: "2vh", marginTop: "2vh"}}></img>
               </div>
             </div>
           </div>
@@ -57,42 +78,49 @@ class Dashboard extends Component {
               className="col s1"
               style={{ height: "85vh", backgroundColor: "#2c2f33", width: "20vh", marginBottom: 0}}
             >
-            <div className="left-align" style={{paddingTop: "10px",paddingLeft: "40px"}}>
-                <div className="left-align">
+            <div className="left-align" style={{paddingTop: "35px",paddingLeft: "40px"}}>
+                <button className="left-align" style={{border:"none",backgroundColor: "#2c2f33", paddingLeft: "0px", marginLeft:"0vh"}} onClick={()=>gotoHome()}>
+		  <div className="left-align">
                 	<img src={HomeLogo} style={{height: "5vh", marginTop: "2vh"}} />
-		</div>
-                <div className="left-align">
-                	<h7 style={{ color: "#7289da", paddingTop: "20px", fontSize: "100%"}}>Home</h7>
-		</div>
+		  </div>
+
+                </button>
             </div>
 
-            <div className="left-align" style={{paddingTop: "10px",paddingLeft: "40px"}}>
+            <div className="left-align" style={{paddingTop: "35px",paddingLeft: "30px"}}>
+                <button className="left-align" style={{border:"none",backgroundColor: "#2c2f33", paddingLeft: "0px", marginLeft:"0vh"}} onClick={()=>gotoSteam()}>
                 <div className="left-align">
-                	<img src={GameLogo} style={{height: "5vh", marginTop: "2vh"}} />
+                	<img src={SteamLogo} style={{height: "5vh", marginTop: "2vh"}} />
                 </div>
-                <div className="left-align">
-                	<h7 style={{color: "#7289da", paddingTop:"20px", fontSize: "100%"}}>Game</h7>
-		</div>
+                </button>
             </div>
 
-            <div className="left-align" style={{paddingTop: "10px", paddingLeft: "40px"}}>
+            <div className="left-align" style={{paddingTop: "35px",paddingLeft: "40px"}}>
+                <button className="left-align" style={{border:"none",backgroundColor: "#2c2f33", paddingLeft: "0px", marginLeft:"0vh"}} onClick={()=>gotoEpic()}>
                 <div className="left-align">
-                	<img src={StreamLogo} style={{height: "5vh", marginTop: "2vh"}} />
+                	<img src={EpicLogo} style={{height: "5vh", marginTop: "2vh"}} />
                 </div>
-                <div className="left-align">
-                	<h7 style={{color: "#7289da", paddingTop:"20px", fontSize: "100%"}}>Stream</h7>
-		</div>
+                </button>
             </div>
 
-            <div className="left-align" style={{paddingTop: "10px", paddingLeft: "40px"}}>
+
+            <div className="left-align" style={{paddingTop: "35px", paddingLeft: "25px"}}>
+                <button className="left-align" style={{border:"none",backgroundColor: "#2c2f33", paddingLeft: "0px", marginLeft:"0vh"}} onClick={()=>gotoTwitch()}>
+                <div className="left-align">
+                	<img src={TwitchLogo} style={{height: "5vh", marginTop: "2vh"}} />
+                </div>
+		</button>
+            </div>
+
+            <div className="left-align" style={{paddingTop: "35px", paddingLeft: "40px"}}>
+                <button className="left-align" style={{border:"none",backgroundColor: "#2c2f33", paddingLeft: "0px", marginLeft:"0vh"}} onClick={()=>gotoHelp()}>
                 <div className="left-align">
                 	<img src={HelpLogo} style={{height: "5vh", marginTop: "2vh"}} />
 		</div>
-                <div className="left-align">
-                	<h7 style={{color: "#7289da", paddingTop:"20px", fontSize: "100%"}}>Help</h7>
-		</div>
+		</button>
             </div>
             </div>
+
             <div className="col s10" style={{ paddingLeft: "5vh" }}>
               <h6 className="left-align">Latest Releases</h6>
               <GamersCarousel className="col s10" ></GamersCarousel>
