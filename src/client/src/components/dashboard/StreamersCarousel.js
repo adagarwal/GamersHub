1990cc50389d6ebca2c9e7c4a7e008796f403419
asengup2 <asengup2@uci.edu>
2020-11-29 20:09:11 -0800

    StreamersCarousel.js async call with wait

--- src/client/src/components/dashboard/StreamersCarousel.js
+++ src/client/src/components/dashboard/StreamersCarousel.js
@@ -1,52 +1,60 @@
 import axios from "axios";
-import React,{useState, useEffect} from "react";
-
+import React, {Component} from "react";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import Slider from "react-slick";
 
-export default function StreamersCarousel() {
-  //const [featuredStreams, updateData] = useState();
-  //useEffect(() => {
-  //  const getData = async() => {
-  //     const resp = await axios.get("/api/twitch/featuredstreams");
-  //     updateData(resp);
-  //  }
-  //  getData();
-  // },[]);
 
-  //window.alert("DATA" + JSON.stringify(featuredStreams));
+class StreamersCarousel extends Component {
+    constructor(){
+        super();
+        this.state = {
+            data: null
+        }
+    }
+    componentDidMount(){
+        const endpoint = "/api/twitch/featuredstreams";
+        const setState = this.setState.bind(this);
+        axios
+            .get(endpoint)
+            .then(res=>{setState({data:res})});
+    }
+    render() {
+        if(this.state.data == null){
+            return(<div>Loading</div>);
+        }
+         //JSON.parse(this.state.data);
+        const {abc} = JSON.parse(JSON.stringify(this.state.data));
+        console.log(abc);
+        return (
+            <div>
+                {
+                    <div className="StreamersCarousel" >
+                            <Slider
+                                dots={true}
+                                slidesToShow={6}
+                                arrows={true}
 
-  //var pobj = JSON.parse(featuredStreams);
-  var resp;
-  axios
-  .get("/api/twitch/featuredstreams")
-  .then(res=>{resp = res; window.alert(JSON.stringify(res))});
-
-  window.alert("RESP" + JSON.stringify(resp));
-
-  return (
-    <div className="StreamersCarousel" >
-      <Slider
-        dots={true}
-        slidesToShow={6}
-        arrows={true}
-
-      >
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
-       <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div> 
- 
+                            >
+                                <div>${JSON.stringify(this.state.data)}</div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
+                                <div ><img src="https://placekitten.com/300/300" className="image" style={{height:"30vh"}}></img></div>
 
 
-      </Slider>
-    </div>
-  );
+
+                            </Slider>
+                        </div>
+                }
+            </div>);
+    }
 }
+
+export default StreamersCarousel;
